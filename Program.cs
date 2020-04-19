8e884fded1ec759c55625941fbabbcaa2fd052df
faha-berdiev <madysoftware@gmail.com>
2020-04-19 14:36:02 +0300

    Создание делегата

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 
 namespace Day_14
 {
+    delegate T Operation<T, K, O>(O val1, K val2);
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Вас приветствует калькулятор.\n\tИспользование: вводите первое число, затем введите (+, -, /, *), а после введите воторое число, после нажатия клавиши Enter вы можете повторить операцию, для вывода ответов операций введите: Вывод.");
+            
+            string cmd = string.Empty;
+            while(cmd != "Вывод")
+            {
+
+            }
+
+            if(cmd == "Вывод")
+            {
+
+            }
         }
     }
 }
