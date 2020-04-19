7a1ae0c7a7874dda308dbc9edadfb06e07ca033e
faha-berdiev <madysoftware@gmail.com>
2020-04-19 15:49:58 +0300

    Функционал вывода добавлен

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day_14
 {
@@ -7,6 +8,9 @@ namespace Day_14
     {
         static void Main(string[] args)
         {
+            List<string> operands = new List<string>(); // Числа для проведения операций разделенные символом '|'
+            List<char> operators = new List<char>(); // Операторы введеные пользователем
+            Operation<double, double, double> operation = null;
             Console.WriteLine("Вас приветствует калькулятор.\n\tИспользование: вводите первое число, затем введите (+, -, /, *), а после введите воторое число, после нажатия клавиши Enter вы можете повторить операцию, для вывода ответов операций введите: Вывод.");
             
             string cmd = string.Empty;
@@ -17,7 +21,16 @@ namespace Day_14
 
             if(cmd == "Вывод")
             {
-
+                if(operands != null)
+                {
+                    for(int i = 0; i < operands.Count - 1; i++)
+                    {
+                        string[] opers = operands[i].Split('|'); // Перевожу числа в массив, что бы перевести их в числовые переменные
+                        double A = double.Parse(opers[0].Replace('.', ',')); // Первый левый операнд
+                        double B = double.Parse(opers[1].Replace('.', ',')); // Второй правый операнд
+                        Console.WriteLine($"{A} {operators[i]} {B} = {operation(A, B)}"); // Вывод данных в консоль
+                    }
+                }
             }
         }
     }
